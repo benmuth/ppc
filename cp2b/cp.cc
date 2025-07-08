ec793a8255ec17ea72c3573ae93c5ca36b9ce125
Ben Muthalaly <benmuthalaly@gmail.com>
2025-07-08 17:58:39 -0400

    Initial implementation using OpenMP for CP2b.
    ./grading benchmark
    
    Running benchmark
    Compiling...
    Compiled
    test                   time  result
    benchmarks/1.txt     0.099s  pass
    
     · I would expect that a typical solution for this task performs at least
       1.00 billion arithmetic operations.
       → It seems you managed to perform at least
       ≈ 10.1 billion useful arithmetic operations per second.
    
     · Your code used 0.099 sec of wallclock time, and 0.564 sec of CPU time
       → you used 5.7 simultaneous hardware threads on average.
    
    benchmarks/2.txt     1.464s  pass
    
     · I would expect that a typical solution for this task performs at least
       16.0 billion arithmetic operations.
       → It seems you managed to perform at least
       ≈ 10.9 billion useful arithmetic operations per second.
    
     · Your code used 1.464 sec of wallclock time, and 7.238 sec of CPU time
       → you used 4.9 simultaneous hardware threads on average.

---
+++ cp2b/cp.cc
@@ -0,0 +1,68 @@
+#include <cmath>
+#include <cstdlib>
+
+/*
+This is the function you need to implement. Quick reference:
+- input rows: 0 <= y < ny
+- input columns: 0 <= x < nx
+- element at row y and column x is stored in data[x + y*nx]
+- correlation between rows i and row j has to be stored in result[i + j*ny]
+- only parts with 0 <= j <= i < ny need to be filled
+*/
+void correlate(int ny, int nx, const float *data, float *result) {
+  int data_size = nx * ny;
+
+  double *normalized = (double *)malloc(data_size * sizeof(double));
+  #pragma omp parallel for
+  for (int i = 0; i < data_size; ++i) {
+    normalized[i] = 0;
+  }
+
+  // normalize input rows (center vector around origin)
+
+  #pragma omp parallel for
+  for (int j = 0; j < ny; ++j) {
+    double row_sum = 0;
+    for (int i = 0; i < nx; ++i) {
+      row_sum += data[i + j * nx];
+    }
+    double row_mean = row_sum / nx;
+
+    for (int i = 0; i < nx; ++i) {
+      normalized[i + (j * nx)] = data[i + (j * nx)] - row_mean;
+    }
+  }
+
+  // normalize input rows (scale vector to length of 1)
+  #pragma omp parallel for
+  for (int j = 0; j < ny; ++j) {
+    double row_square_sum = 0;
+    for (int i = 0; i < nx; ++i) {
+      double val = normalized[i + j * nx];
+      row_square_sum += val * val;
+    }
+    double row_magnitude = sqrt(row_square_sum);
+
+    for (int i = 0; i < nx; ++i) {
+      normalized[i + (j * nx)] = normalized[i + (j * nx)] / row_magnitude;
+    }
+  }
+
+  // calculate upper triangle of matrix product (normalized matrix) x
+  // (transpose)
+  // this is taking the dot product of each pairwise row (which gives the
+  // correlation) first, iterate over pairs of rows (upper triangle)
+  #pragma omp parallel for
+  for (int i = 0; i < ny; ++i) {
+    for (int j = i; j < ny; ++j) {
+      double correlation = 0;
+      // second, iterate through rows and sum products of elements (dot product)
+      for (int k = 0; k < nx; ++k) {
+        correlation += normalized[i * nx + k] * normalized[j * nx + k];
+      }
+      result[j + i * ny] = correlation;
+    }
+  }
+
+  free(normalized);
+}
