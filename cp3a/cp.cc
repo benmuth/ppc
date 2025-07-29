6dc9134c0f28e9f81bb45a3f2552d045ae358417
Ben Muthalaly <benmuthalaly@gmail.com>
2025-07-29 05:41:44 -0400

    Initial attempt at cp3a with SIMD and threads.
    ./grading benchmark
    
    test                    time  result
    benchmarks/1.txt      0.025s  pass
    
     · I would expect that a typical solution for this task performs at least
       1.00 billion arithmetic operations.
       → It seems you managed to perform at least
       ≈ 39.4 billion useful arithmetic operations per second.
    
     · Your code used 0.025 sec of wallclock time, and 0.212 sec of CPU time
       → you used 8.3 simultaneous hardware threads on average.
    
    benchmarks/2a.txt     0.328s  pass
    
     · I would expect that a typical solution for this task performs at least
       16.0 billion arithmetic operations.
       → It seems you managed to perform at least
       ≈ 48.9 billion useful arithmetic operations per second.
    
     · Your code used 0.328 sec of wallclock time, and 2.880 sec of CPU time
       → you used 8.8 simultaneous hardware threads on average.
    
    benchmarks/2b.txt     0.322s  pass
    
     · I would expect that a typical solution for this task performs at least
       16.0 billion arithmetic operations.
       → It seems you managed to perform at least
       ≈ 49.6 billion useful arithmetic operations per second.
    
     · Your code used 0.322 sec of wallclock time, and 2.939 sec of CPU time
       → you used 9.1 simultaneous hardware threads on average.
    
    benchmarks/2c.txt     0.325s  pass
    
     · I would expect that a typical solution for this task performs at least
       16.0 billion arithmetic operations.
       → It seems you managed to perform at least
       ≈ 49.4 billion useful arithmetic operations per second.
    
     · Your code used 0.325 sec of wallclock time, and 2.937 sec of CPU time
       → you used 9.1 simultaneous hardware threads on average.
    
    benchmarks/3.txt      6.426s  pass
    
     · I would expect that a typical solution for this task performs at least
       216 billion arithmetic operations.
       → It seems you managed to perform at least
       ≈ 33.6 billion useful arithmetic operations per second.
    
     · Your code used 6.426 sec of wallclock time, and 56.569 sec of CPU time
       → you used 8.8 simultaneous hardware threads on average.
    
    benchmarks/4.txt     23.963s  pass
    
     · I would expect that a typical solution for this task performs at least
       729 billion arithmetic operations.
       → It seems you managed to perform at least
       ≈ 30.4 billion useful arithmetic operations per second.
    
     · Your code used 23.963 sec of wallclock time, and 197.334 sec of CPU time
       → you used 8.2 simultaneous hardware threads on average.
    
    Instruments metrics:
    Samples                : 173,821
    Total L1D_CACHE_MISS_LD: 121,018,947,937
    Total L1D_CACHE_MISS_ST: 23,688,317,905
    Total INST_SIMD_LD     : 137,658,611,717
    Total INST_SIMD_ST     : 30,596,930
    Total INST_SIMD_ALU    : 137,872,742,278

---
+++ cp3a/cp.cc
@@ -0,0 +1,108 @@
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+
+typedef double double4_t __attribute__((vector_size(4 * sizeof(double))));
+
+/*
+This is the function you need to implement. Quick reference:
+- input rows: 0 <= y < ny
+- input columns: 0 <= x < nx
+- element at row y and column x is stored in data[x + y*nx]
+- correlation between rows i and row j has to be stored in result[i + j*ny]
+- only parts with 0 <= j <= i < ny need to be filled
+*/
+void correlate(int ny, int nx, const float *data, float *result) {
+  constexpr int elems_per_vec = 4;
+
+  int elems_per_padded_row =
+      (nx + elems_per_vec - 1) / elems_per_vec * elems_per_vec;
+  int vectors_per_padded_row = elems_per_padded_row / elems_per_vec;
+  // int data_size = elems_per_padded_row * ny;
+
+  double4_t zero_vec = {0.0, 0.0, 0.0, 0.0};
+  std::vector<double4_t> vnormalized(vectors_per_padded_row * ny, zero_vec);
+  std::vector<double4_t> vdata(vectors_per_padded_row * ny, zero_vec);
+
+  // for (int j = 0; j < ny; ++j) {
+  // }
+
+  // normalize input rows (center vector of length 1 around origin)
+  #pragma omp parallel for
+  for (int j = 0; j < ny; ++j) {
+    double4_t sum_vec = {0.0, 0.0, 0.0, 0.0};
+    int row_start_vec = j * vectors_per_padded_row;
+    // pack data vectors
+    for (int i = 0; i < nx; ++i) {
+      int vector_idx = row_start_vec + i / elems_per_vec;
+      int elem_idx = i % elems_per_vec;
+      vdata[vector_idx][elem_idx] = data[i + j * nx];
+    }
+
+    // sum vectors
+    for (int i = 0; i < vectors_per_padded_row; ++i) {
+      sum_vec += vdata[i + row_start_vec];
+    }
+    double row_sum = sum_vec[0] + sum_vec[1] + sum_vec[2] + sum_vec[3];
+    // calculate mean
+    double row_mean = row_sum / nx;
+
+    // subtract mean
+    double4_t mean_vec = {row_mean, row_mean, row_mean, row_mean};
+    for (int i = 0; i < vectors_per_padded_row; ++i) {
+      vnormalized[i + row_start_vec] = vdata[i + row_start_vec] - mean_vec;
+    }
+
+    // set padded elements to 0
+    if (nx % elems_per_vec != 0) {
+      int padding = nx % elems_per_vec;
+      for (int k = padding; k < elems_per_vec; ++k) {
+        vnormalized[row_start_vec + vectors_per_padded_row - 1][k] = 0.0;
+      }
+    }
+    
+    // square elems
+    double4_t row_square_sum_vec = {0.0, 0.0, 0.0, 0.0};
+    for (int i = 0; i < vectors_per_padded_row; ++i) {
+      double4_t val = vnormalized[i + row_start_vec];
+      row_square_sum_vec += val * val;
+    }
+
+    double row_square_sum = row_square_sum_vec[0] + row_square_sum_vec[1] +
+                            row_square_sum_vec[2] + row_square_sum_vec[3];
+    double row_magnitude = sqrt(row_square_sum);
+
+    double4_t row_magnitude_vec = {1 / row_magnitude, 1 / row_magnitude,
+                                   1 / row_magnitude, 1 / row_magnitude};
+    // normalize
+    for (int i = 0; i < vectors_per_padded_row; ++i) {
+      vnormalized[i + row_start_vec] *= row_magnitude_vec;
+    }
+  }
+
+  // calculate upper triangle of matrix product (normalized matrix) x
+  // (transpose)
+  // this is taking the dot product of each pairwise row (which gives the
+  // correlation) first, iterate over pairs of rows (upper triangle)
+  #pragma omp parallel for schedule(dynamic, 1)
+  for (int i = 0; i < ny; ++i) {
+    int row_i_start = i * vectors_per_padded_row;
+    int i_ny = i * ny;
+    for (int j = i; j < ny; ++j) {
+      int row_j_start = j * vectors_per_padded_row;
+      // iterate through the rows and sum the products of elements (dot product)
+      double4_t correlation = {0.0, 0.0, 0.0, 0.0};
+
+      // std::cerr << "row i start " << row_i_start << std::endl;
+      // std::cerr << "row j start " << row_j_start << std::endl;
+      for (int k = 0; k < vectors_per_padded_row; ++k) {
+        correlation +=
+            vnormalized[row_i_start + k] * vnormalized[row_j_start + k];
+      }
+
+      result[j + i_ny] =
+          correlation[0] + correlation[1] + correlation[2] + correlation[3];
+    }
+  }
+}
